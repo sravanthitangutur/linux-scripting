23076730bdd1eeec0b173567978480264424aef0
sravanthitangutur <sravanthi.tangutur@hcl.com>
2023-01-06 11:39:00 +0000

    first commit

---
+++ test2.sh
@@ -0,0 +1,11 @@
+Name="Satyajit"
+case "$Name" in
+    #case 1
+    "Rajib") echo "Profession : Software Engineer" ;;
+    
+    #case 2
+    "Vikas") echo "Profession : Web Developer" ;;
+    
+    #case 3
+    "Satyajit") echo "Profession : Technical Content Writer" ;;
+esac
